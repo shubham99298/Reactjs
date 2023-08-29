de24d70e24834a8b19d413e5724e90d28bd70f7f
Shubham <shubhamlata2002@gmail.com>
2023-08-29 20:23:05 +0530

    reactjs

--- src/component/Acomponent.js
+++ src/component/Acomponent.js
@@ -6,7 +6,7 @@ import DataContext from './DataContext';
 const Acomponent = () => {
     const vashihathnama="4000 cr";
   return (
-    
+  
     <DataContext.Provider value={vashihathnama}>
         <div>
         Acomponent
